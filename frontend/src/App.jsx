0af3aab7442ee927f4718fd5924c740d1d3c948a
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2026-01-03 18:09:43 +0545

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
+import About from "./Pages/About";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <p className="read-the-docs bg-amber-200 w-full">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
