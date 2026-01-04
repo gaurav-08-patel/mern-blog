92e178dff742814225c56f6616e7ca6ae494752e
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-01-04 21:31:35 +0545

    add functionality to signup page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:3500",
+                secure: false,
+            },
+        },
+    },
     plugins: [tailwindcss(), react(), flowbiteReact()],
 });
