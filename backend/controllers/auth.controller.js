ced5014b36e5a052a7b9939e098d2b2db5549551
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2026-01-05 13:17:20 +0545

    create signin api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
@@ -18,4 +19,35 @@ export const signup = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
+};
+
+export const signin = async (req, res) => {
+    let { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required ." });
+    }
+
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+        return res.status(404).json({ message: "User not found ." });
+    }
+
+    const matchPass = bcryptjs.compareSync(password, validUser.password);
+
+    if (!matchPass) {
+        return res.status(400).json({ message: "Invalid credentials !" });
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+    });
+
+    let { password: pass, ...rest } = validUser._doc;
+    res.status(200)
+        .cookie("access_token", token, {
+            httpOnly: true,
+        })
+        .json(rest);
 };
