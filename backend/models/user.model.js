c5d1859298fee221b91b1677270ba09533a23356
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-01-05 16:21:58 +0545

    added profilePicture to model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        profilePicture: {
+            type: String,
+            default:
+                "https://static.vecteezy.com/system/resources/thumbnails/032/176/191/small/business-avatar-profile-black-icon-man-of-user-symbol-in-trendy-flat-style-isolated-on-male-profile-people-diverse-face-for-social-network-or-web-vector.jpg",
+        },
     },
     {
         timestamps: true,
