851b4c8bc1934c2622aa0795e4eeb8fdf2388ba6
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2026-01-06 16:32:37 +0545

    tried creating dark mode toggle but failed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <AuthContextProvider>
-            <App />
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
         </AuthContextProvider>
     </StrictMode>
 );
