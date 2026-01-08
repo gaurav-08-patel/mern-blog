6748ef368bcecb33a200a8c533efc42578bd48ef
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2026-01-08 18:18:27 +0545

    create dashboard

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,11 +1,31 @@
-
+import { useEffect, useState } from "react";
+import DashProfile from "../components/DashProfile";
+import DashSidebar from "../components/DashSidebar";
+import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+    const location = useLocation();
+    console.log(location);
+    let [tab, setTab] = useState("");
 
-export default Dashboard
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const urlTab = params.get("tab");
+        if (urlTab) {
+            setTab(urlTab);
+        }
+    }, [location.search]);
+
+    return (
+        <div className="min-h-screen flex flex-col md:flex-row">
+            {/* Sidebar */}
+            <div>
+                <DashSidebar />
+            </div>
+            {/* Profile.... */}
+            <div>{tab === "profile" && <DashProfile />}</div>
+        </div>
+    );
+};
+
+export default Dashboard;
