000c3d6fd90cbceda63db706c6a3cf381caa0077
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2026-01-08 19:07:15 +0545

    made custom Theme for sidebar

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -5,13 +5,31 @@ import {
     SidebarItemGroup,
     SidebarItems,
 } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { HiUser } from "react-icons/hi";
+import { useEffect, useState } from "react";
+
+const customTheme = {
+    item: {
+        active: "bg-gray-200 text-black",
+    },
+};
 
 const DashSidebar = () => {
+    const location = useLocation();
+    let [tab, setTab] = useState("");
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const urlTab = params.get("tab");
+        if (urlTab) {
+            setTab(urlTab);
+        }
+    }, [location.search]);
+
     return (
         <div className="md:h-full">
-            <Sidebar className="w-full md:w-56 lg:w-70 ">
+            <Sidebar className="w-full md:w-56 lg:w-70 " theme={customTheme}>
                 <SidebarItems className="">
                     <SidebarItemGroup>
                         <Link to={"/dashboard?tab=profile"}>
@@ -19,6 +37,7 @@ const DashSidebar = () => {
                                 icon={HiUser}
                                 label="User"
                                 labelColor="dark"
+                                active={tab === "profile"}
                             >
                                 Profile
                             </SidebarItem>
