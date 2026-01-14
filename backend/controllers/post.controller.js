72297e0d07bcf37a2632cab2017caf939aa02304
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-01-14 13:15:56 +0545

    create api route for getposts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -30,3 +30,51 @@ export const create = async (req, res) => {
         res.status(400).json({ error: "Duplicate Key" });
     }
 };
+
+export const getPosts = async (req, res) => {
+    console.log("api posts");
+
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.startIndex) || 9;
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: "i" } },
+                    {
+                        content: {
+                            $regex: req.query.searchTerm,
+                            $options: "i",
+                        },
+                    },
+                ],
+            }),
+        })
+            .sort({ updatedAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.post('/create',verifyToken, create);
+router.get('/getposts', getPosts);
 
 export default router
