b4724fc23d12374fcaf190ba95fb4c681499a9ca
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-01-17 20:42:17 +0545

    fixed error in comment.controller.js

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,9 +1,9 @@
-import Comment from "../models/comment.model";
+import Comment from "../models/comment.model.js";
 
 export const createComment = async (req, res) => {
     let { content, userId, postId } = req.body;
 
-    if (req.user.id !== req.userId) {
+    if (req.user.id !== userId) {
         res.status(403).json({
             error: "You are not allowed to create comment.",
         });
@@ -21,5 +21,5 @@ export const createComment = async (req, res) => {
         res.status(200).json(newComment);
     } catch (error) {
         res.status(400).json(error.message);
-    }
+    } 
 };
