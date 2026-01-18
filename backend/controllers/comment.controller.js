50ef3028955bc86f3f2eb79b9f7060638c991f1c
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-01-18 15:32:58 +0545

    create likeComment API route

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -28,9 +28,33 @@ export const getComments = async (req, res) => {
     try {
         let comments = await Comment.find({ postId: req.params.postId })
             .sort({ createdAt: -1 })
-            .populate("userId" , "_id username email profilePicture");
+            .populate("userId", "_id username email profilePicture");
 
         res.status(200).json(comments);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+};
+
+export const likeComment = async (req, res) => {
+    try {
+        let comment = await Comment.findById(req.params.commentId);
+
+        if (!comment) {
+            res.status(404).json("Comment not found.");
+        }
+
+        const userIndex = comment.likes.indexOf(req.user.id);
+
+        if (userIndex === -1) {
+            comment.numberOfLikes += 1;
+            comment.likes.push(req.user.id);
+        } else {
+            comment.numberOfLikes -= 1;
+            comment.likes.splice(userIndex, 1);
+        }
+        await comment.save();
+        res.status(200).json(comment);
     } catch (error) {
         res.status(400).json(error.message);
     }
--- backend/routes/comment.routes.js
+++ backend/routes/comment.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyToken.js";
-import { createComment, getComments } from "../controllers/comment.controller.js";
+import {
+    createComment,
+    getComments,
+    likeComment,
+} from "../controllers/comment.controller.js";
 
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getComments);
+router.put("/likeComment/:commentId",verifyToken, likeComment);
 
 export default router;
