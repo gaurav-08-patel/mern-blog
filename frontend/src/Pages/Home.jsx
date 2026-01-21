f2c5b39fa583add54babb4058a2512bb88d91bc2
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-01-21 21:19:57 +0545

    made articles layout responsive

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -46,10 +46,10 @@ const Home = () => {
 
             <div className="mt-10 max-w-5xl  mx-auto flex flex-col mb-5 p-3">
                 <h1 className="text-2xl text-center my-4 font-semibold">Recent Articles</h1>
-                <div className="flex flex-wrap justify-between gap-4">
+                <div className="flex flex-wrap justify-center lg:justify-between md:bg-amber-100 gap-4">
                     {recentPosts &&
                         recentPosts.map((post) => (
-                            <PostCard key={post._id} post={post} />
+                            <PostCard key={post._id} post={post} className="sm:w-full md:w-100"/>
                         ))}
                 </div>
                 <span className="font-semibold text-xl text-teal-500 hover:underline mt-4 text-center">
